ab8ec51323b5f8d8bf18819e466de20b765e69d4
alen <592453675@qq.com>
2024-09-06 18:10:22 +0800

    no message

--- src/core/config.ts
+++ src/core/config.ts
@@ -8,28 +8,49 @@ import type { Component } from 'types/component'
  */
 export interface Config {
   // 用户
+
+  // 合并策略
   optionMergeStrategies: { [key: string]: Function }
+  // 是否禁止显示警告
   silent: boolean
+  // 启动时显示生产模式提示消息？
   productionTip: boolean
+  // 是否启用性能
   performance: boolean
+  // 是否启用 devtools
   devtools: boolean
+  // 观察程序错误的错误处理程序
   errorHandler?: (err: Error, vm: Component | null, info: string) => void
+  // 用于观察程序警告的 Warn 处理程序
   warnHandler?: (msg: string, vm: Component | null, trace: string) => void
+  //  忽略自定义元素
   ignoredElements: Array<string | RegExp>
+  // 自定义密钥别名
   keyCodes: { [key: string]: number | Array<number> }
 
   // 平台
+
+  // 检查标记是否为保留的，以便无法将其注册为组件。
   isReservedTag: (x: string) => boolean | undefined
+  // 检查是否保留了 attribute ，使其不能用作 component prop。
   isReservedAttr: (x: string) => true | undefined
+  // 解析平台标签名称
   parsePlatformTagName: (x: string) => string
+  // 检查是否为未知元素
   isUnknownElement: (x: string) => boolean
+  // 获取元素的命名空间
   getTagNamespace: (x: string) => string | undefined
+  // 检查是否必须使用 property 绑定 attribute，例如 value
   mustUseProp: (tag: string, type?: string | null, name?: string) => boolean
 
   // 私人
+
+  // 异步执行更新。
   async: boolean
 
   // 遗产
+
+  // 生命周期钩子
   _lifecycleHooks: Array<string>
 }
 
